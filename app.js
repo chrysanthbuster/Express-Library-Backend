73d5f4eeb7f1f61822479b01b0cc79ff6f5b8502
RYMND <39932526+chrysanthbuster@users.noreply.github.com>
2021-06-17 22:54:59 -0400

    added dependencies

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+"use strict";
+
+require('dotenv').config(); //load additional setting into environment
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -16,8 +19,7 @@ const app = express();
 
 // Set up mongoose connection, using local database for this project
 const mongoose = require("mongoose");
-const dev_db_url = require("./config").connectionString; //config.connectionString
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
@@ -44,18 +46,18 @@ app.use("/catalog", catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
