a357d1cf7812411609619125d778e8ce270981d2
RYMND <39932526+chrysanthbuster@users.noreply.github.com>
2021-07-13 23:56:44 -0400

    update dependency, and code format

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Express-Library
+
 Product made learning from http://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
 
 Read the full story here: https://docs.google.com/document/d/160sK8xRfuzKBKELEnyQaGr-97wLOD7wu4-SZHbT2RNw/edit?usp=sharing
 
 # setup enviroment
+
 Intall npm packages npm install
 
 Install local mongodb database, create a LocalLibraryDB database and a cluster name (name dont matter) then generate connection string and update the connnection string in configuration file
 
-run the setup db with node populatedb 
+run the setup db with node populatedb
 
 generate PEM with openssl
 
 # start the application
-$ENV:DEBUG = "express-locallibrary-tutorial:*"; npm start
 
+$ENV:DEBUG = "express-locallibrary-tutorial:\*"; npm start
--- app.js
+++ app.js
@@ -1,63 +1,63 @@
-"use strict";
+'use strict';
 
 require('dotenv').config(); //load additional setting into environment
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const helmet = require("helmet");
-const compression = require("compression");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
 
 //Routers
-const indexRouter = require("./routes/index");
+const indexRouter = require('./routes/index');
 //const usersRouter = require("./routes/users");  //disable user routing
-const catalogRouter = require("./routes/catalog");
+const catalogRouter = require('./routes/catalog');
 //const contactRouter = require("./routes/contact");
 
 const app = express();
 
 // Set up mongoose connection, using local database for this project
-const mongoose = require("mongoose");
-const mongoDB = process.env.MONGODB_URI 
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(helmet());
-app.use(compression()); 
+app.use(compression());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 //app.use("/users", usersRouter);    //disable user routing
-app.use("/catalog", catalogRouter);  // Add catalog routes to middleware chain.
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
 //app.use("/contact", contactRouter);  // Contact page
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render("error");
+    res.render('error');
 });
 
 module.exports = app;
